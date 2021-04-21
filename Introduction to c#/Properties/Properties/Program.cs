b0a497a0e1e73c1757d47be899ad3b6556d654b7
prasad151027 <66762086+prasad151027@users.noreply.github.com>
2021-04-21 15:44:31 +0530

    Add files via upload

---
+++ Introduction to c#/Properties/Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        public class student
+        {
+            public int ID;
+            public string Name;
+            public int PassMark = 35;
+        }
+        static void Main(string[] args)
+        {
+            student c1 = new student();
+            c1.ID = -101;
+            c1.Name = null;
+            c1.PassMark = 0;
+            Console.WriteLine("ID={0} && Name={1} && PassMark={2}", c1.ID, c1.Name, c1.PassMark);
+        }
+    }
+}
