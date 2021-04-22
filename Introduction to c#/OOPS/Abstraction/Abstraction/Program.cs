0091be802df4e281c3564a9a1f599f2fa47bf016
prasad151027 <66762086+prasad151027@users.noreply.github.com>
2021-04-22 15:04:35 +0530

    Add files via upload

---
+++ Introduction to c#/OOPS/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+    }
+    public class Rectangle:Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+
+        }
+    }
+}
