018556fb20edfef4c42cd3fe83249968ee7d7463
prasad151027 <66762086+prasad151027@users.noreply.github.com>
2021-04-22 11:33:40 +0530

    Add files via upload

---
+++ Introduction to c#/OOPS/Method Overloading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Employee
+    {
+        public string name;
+        public float salary;
+        public static string company = "Sails Software solutions";
+        public Employee(string name,float salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+        public void address(string village,string mandal)
+        {
+            Console.WriteLine("Name:{0}\n Salary:{1}\n Company:{2}\n village:{3}\n mandal:{4}\n ", name, salary,company, village, mandal);
+        }
+        public void address(string village,string mandal,string dist)
+        {
+            Console.WriteLine("Name:{0}\n Salary:{1}\n Company:{2}\n village:{3}\n mandal:{4}\n District:{5}\n ", name, salary,company,village, mandal,dist);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Employee e1 = new Employee("Prasad" ,30000.00f);
+            Employee e2 = new Employee("Mohan", 25000.00f);
+            e1.address("Rana", "Jalumuru");
+            e2.address("Undi", "Bheemavarm", "West Godavari");
+
+
+
+        }
+    }
+}
---
+++ Introduction to c#/OOPS/Method Overriding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class Animal
+    {
+        public string color = "White";
+        public virtual void eat()
+        {
+            Console.WriteLine("eating");
+        }
+    }
+    public class Dog : Animal
+    {
+        public string color = "Black";
+        public  void bark()
+        {
+            Console.WriteLine("Barking");
+        }
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread");
+
+        }
+        public void Showcolor()
+        {
+            Console.WriteLine("Base class color:"+ base.color);//base keyword is used to access the base class members
+            Console.WriteLine("Derived class colr:"+color);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.eat();
+            d1.bark();
+            d1.Showcolor();
+        }
+    }
+}
